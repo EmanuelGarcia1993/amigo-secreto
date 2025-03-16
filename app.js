3f2eddfa64256f3dac742a1aa06aa9434295e5d4
Emanuel Alejandro Garcia <e.m.g.linkinpark@gmail.com>
2025-03-16 00:23:22 -0300

    Se agrega la funcion para limpiar la caja  el almacenado del nombre en la lista

--- app.js
+++ app.js
@@ -11,8 +11,30 @@ function agregarAmigo() {
     //comprueba que no sea un espacio en blanco y no contenga numeros
     //'/\d/'= regex; busca un numero / '.test' comprueba en la variable lo solicitado
     if (nombre === '' || /\d/.test(nombre)) {
-        //En el caso de que lo ingresado no sea un nombre tira un mensaje de alerta
-        alert('Ingresa un nombre valido');
-    }
+        //En el caso de que lo ingresado no sea un nombre modifica el texto del elemento h2
+        asignarTextoElemento('h2', 'Ingresa un nombre valido');
+        limpiarCaja();
+    } else {
+        //Se agrega el nombre ingresado en la lista
+        listaDeAmigos.push(nombre);
+        limpiarCaja();
+    }  
+
+    console.log(listaDeAmigos);
+}
+
+//Esta funcion asigna un texto al elemento que se quiera modificar
+function asignarTextoElemento(elemento, texto) {
+
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+
+}
+
+//Esta funcion es para limpiar la caja
+function limpiarCaja() {
+
+    document.querySelector('#amigo').value = '';
 
 }
