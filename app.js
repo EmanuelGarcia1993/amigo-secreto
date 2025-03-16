0b1b042889e2b2f8a35514d2578a33258043bda0
Emanuel Alejandro Garcia <e.m.g.linkinpark@gmail.com>
2025-03-15 21:00:53 -0300

    Se agrega en app.js las lineas de codigo de verificacion de datos ingresados por el usuario

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Lista en donde se va a guardar los nombres
+let listaDeAmigos = [];
+
+//Ingreso de nombres
+function agregarAmigo() {
+
+    //toma el valor ingresado en el input con id:'amigos' y lo guarda en 'nombre'
+    let nombre = document.getElementById('amigo').value;
+
+    //comprueba que no sea un espacio en blanco y no contenga numeros
+    //'/\d/'= regex; busca un numero / '.test' comprueba en la variable lo solicitado
+    if (nombre === '' || /\d/.test(nombre)) {
+        //En el caso de que lo ingresado no sea un nombre tira un mensaje de alerta
+        alert('Ingresa un nombre valido');
+    }
+
+}
